5fc9173373cc2a8a5408671ce8ba80d9be092dde
ㅤ <>
2022-09-13 22:54:13 +0100

    updated the highlighting

--- docs/main.js
+++ docs/main.js
@@ -244,5 +244,30 @@
     }
   }
   when_ready(main);
+
+  // usr/script.js
+  function highlight() {
+    if (document.querySelector(".page a")) {
+      let body_classes = document.body.classList;
+      body_classes.add("highlighting-active", "highlighting-first");
+      document.addEventListener("scroll", function() {
+        if (body_classes.contains("highlighting-first")) {
+          this.removeEventListener("click", arguments.callee);
+          body_classes.remove("highlighting-active", "highlighting-first");
+        }
+      });
+    }
+  }
+  function main2() {
+    highlight();
+  }
+  function when_ready2(f) {
+    if (document.readyState != "loading") {
+      f();
+    } else {
+      document.addEventListener("DOMContentLoaded", f);
+    }
+  }
+  when_ready2(main2);
 })();
 /*! @source http://purl.eligrey.com/github/classList.js/blob/master/classList.js */
